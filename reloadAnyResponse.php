3f3555b3f08d2de34c4fa617e30cdef36181bfa2
Denis Chenu <denis@sondages.pro>
2019-10-17 23:08:44 +0200

    [dev] 4.0 compat at your own risk

--- reloadAnyResponse.php
+++ reloadAnyResponse.php
@@ -5,7 +5,7 @@
  * @author Denis Chenu <denis@sondages.pro>
  * @copyright 2018-2019 Denis Chenu <http://www.sondages.pro>
  * @license AGPL v3
- * @version 1.4.0
+ * @version 1.4.1
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
